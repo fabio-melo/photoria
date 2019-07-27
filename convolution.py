20265106c69c9ec26b2d6ef0ba5a11a87dcab271
fabio-melo <fabiomelo@cc.ci.ufpb.br>
2019-07-27 14:43:55 -0300

    otimização de código, quatro vezes mais rápido

--- convolution.py
+++ convolution.py
@@ -2,6 +2,7 @@
 import time
 import numpy as np
 from utils import check_bounds
+from functools import lru_cache
 
 class ConvolutionalMask():
   def __init__(self, file = False):
@@ -24,6 +25,7 @@ class ConvolutionalMask():
   def mirror(self):
     self.matrix = np.array([w[::-1] for w in self.matrix[::-1]])
 
+
   def apply_on_pixel_dot(self,image,x,y):
     #cria 
     mtx_sl = np.zeros((self.height, self.width, 3))
@@ -37,6 +39,32 @@ class ConvolutionalMask():
     mtx = mtx_sl * self.matrix
     mtx = np.sum(mtx, axis=(0,1))
 
+    return mtx
+    
+  def fast_apply(self,image):
+    mtx = np.zeros((image.matrix.shape))
+
+    t0 = time.time()
+    for y in range(self.height,image.height - self.height):
+      for x in range(self.width, image.width - self.width):
+        res = image.matrix[y-self.floor_h:y+self.ceil_h, x-self.floor_w:x+self.ceil_w] * self.matrix
+        mtx[y][x] = np.sum(res,axis=(0,1))
+
+    #CANTOS
+    for y in range(self.height):
+      for x in range(self.width):
+        mtx[y][x] = self.apply_on_pixel_dot(image,x,y)
+
+    for y in range(image.height - self.height, image.height):
+      for x in range(image.width - self.width, image.width):
+        mtx[y][x] = self.apply_on_pixel_dot(image,x,y)
+    
+    mtx = np.clip(mtx, a_min=0, a_max=255)
+    mtx = np.round(mtx)
+    mtx = mtx.astype(int)
+    
+    t1 = time.time()
+    print(f"tempo total {t1-t0}")
     return mtx
     
 
@@ -56,6 +84,29 @@ class ConvolutionalMask():
     t1 = time.time()
     print(f"tempo total {t1-t0}")
     return mtx
+
+
+
+from image import ImageMatrix
+from median import MedianMask
+
+media = ConvolutionalMask("masks/media_3x3.txt")
+gauss = ConvolutionalMask("masks/gauss_3x3.txt")
+median = MedianMask()
+y = ImageMatrix("images/aaa1.jpg")
+
+media.apply(y)
+media.fast_apply(y)
+
+#import matplotlib.pyplot as plt
+
+#_, ax = plt.subplots(2,2)
+#ax[0,0].imshow(y.matrix)
+#ax[0,1].imshow(media.fast_apply(y))
+#ax[1,0].imshow(gauss.fast_apply(y))
+#ax[1,1].imshow(median.fast_apply(y))
+#plt.show()
+#%%
 
 
 
